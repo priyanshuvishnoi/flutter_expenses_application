1569339d7cda6c972c624f0b1c683e786cbb2b76
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-04-08 21:44:40 +0530

    platform adaptive

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_application_1/widgets/chart.dart';
@@ -12,6 +14,8 @@ void main() {
   runApp(MyApp());
 }
 
+final platform = Platform.isIOS;
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,6 +24,7 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        // platform: TargetPlatform.iOS,
         primarySwatch: Colors.amber,
         accentColor: Colors.blue,
         fontFamily: 'Quicksand',
@@ -119,6 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: ctx,
       builder: (bCtx) => NewTransaction(_addNewTransaction),
     );
@@ -132,65 +138,122 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final appBar = AppBar(
-      title: Text(
-        'Personal Expenses',
-        style: TextStyle(
-          fontFamily: 'Open Sans',
-        ),
-      ),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.add_circle_outline_outlined),
-          onPressed: () => _startAddNewTransaction(context),
-        ),
-      ],
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+    final PreferredSizeWidget appBar = platform
+        ? CupertinoNavigationBar(
+            middle: Text(
+              'Personal Expenses',
+              style: TextStyle(
+                fontFamily: 'Open Sans',
+              ),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add),
+                  onTap: () => _startAddNewTransaction(context),
+                )
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text(
+              'Personal Expenses',
+              style: TextStyle(
+                fontFamily: 'Open Sans',
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add_circle_outline_outlined),
+                onPressed: () => _startAddNewTransaction(context),
+              ),
+            ],
+          );
+    final txListWidget = Container(
+      height: (mediaQuery.size.height -
+              appBar.preferredSize.height -
+              mediaQuery.padding.top) *
+          .7,
+      child: TransactionList(_userTransaction, _deleteTransaction),
     );
 
-    return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
+    final pageBody = SafeArea(
+      child: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Show Chart'),
-                Switch(
-                  value: _showChart,
-                  onChanged: (val) {
-                    setState(() {
-                      _showChart = val;
-                    });
-                  },
-                ),
-              ],
-            ),
-            _showChart
-                ? Container(
-                    height: (MediaQuery.of(context).size.height -
-                            appBar.preferredSize.height -
-                            MediaQuery.of(context).padding.top) *
-                        0.7,
-                    child: Chart(_recentTransactions),
-                  )
-                : Container(
-                    height: (MediaQuery.of(context).size.height -
-                            appBar.preferredSize.height -
-                            MediaQuery.of(context).padding.top),
-                    child:
-                        TransactionList(_userTransaction, _deleteTransaction),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Show Chart',
+                    style: Theme.of(context).textTheme.title,
                   ),
+                  platform
+                      ? CupertinoSwitch(
+                          activeColor: Theme.of(context).accentColor,
+                          value: _showChart,
+                          onChanged: (val) {
+                            setState(() {
+                              _showChart = val;
+                            });
+                          },
+                        )
+                      : Switch(
+                          activeColor: Theme.of(context).accentColor,
+                          value: _showChart,
+                          onChanged: (val) {
+                            setState(() {
+                              _showChart = val;
+                            });
+                          },
+                        ),
+                ],
+              ),
+            if (!isLandscape)
+              Container(
+                height: (mediaQuery.size.height -
+                        appBar.preferredSize.height -
+                        mediaQuery.padding.top) *
+                    0.3,
+                child: Chart(_recentTransactions),
+              ),
+            if (!isLandscape) txListWidget,
+            if (isLandscape)
+              _showChart
+                  ? Container(
+                      height: (mediaQuery.size.height -
+                              appBar.preferredSize.height -
+                              mediaQuery.padding.top) *
+                          0.7,
+                      child: Chart(_recentTransactions),
+                    )
+                  : txListWidget,
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
     );
+    return platform
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: appBar,
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: pageBody,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: platform
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () => _startAddNewTransaction(context),
+                  ),
+          );
   }
 }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/adaptive_flat_button.dart';
 import 'package:intl/intl.dart';
+
+final platform = Platform.isIOS;
 
 class NewTransaction extends StatefulWidget {
   final addTx;
@@ -26,79 +31,139 @@ class _NewTransactionState extends State<NewTransaction> {
     if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null)
       return;
 
-    widget.addTx(
-      enteredTitle,
-      enteredAmount,
-      _selectedDate
-    );
+    widget.addTx(enteredTitle, enteredAmount, _selectedDate);
 
     Navigator.of(context).pop();
   }
 
   void _presentDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2021),
-      lastDate: DateTime.now(),
-    ).then((pickedDate) {
-      if (pickedDate == null) {
-        return;
-      }
-      setState(() {
-        _selectedDate = pickedDate;
-      });
-    });
+    platform
+        ? showCupertinoModalPopup(
+            context: context,
+            builder: (_) => Container(
+                  height: 300,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 200,
+                        child: CupertinoDatePicker(
+                            mode: CupertinoDatePickerMode.date,
+                            initialDateTime: DateTime.now(),
+                            onDateTimeChanged: (val) {
+                              setState(() {
+                                _selectedDate = val;
+                              });
+                            }),
+                      ),
+
+                      // Close the modal
+                      CupertinoButton(
+                        child: Text('OK'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  ),
+                ))
+        : showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2021),
+            lastDate: DateTime.now(),
+          ).then((pickedDate) {
+            if (pickedDate == null) {
+              return;
+            }
+            setState(() {
+              _selectedDate = pickedDate;
+            });
+          });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              onSubmitted: (_) => _submitData(),
-              controller: _titleController,
-              // onChanged: (value) => titleInput = value,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitData(),
-              // onChanged: (value) => amountInput = value,
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(_selectedDate == null
-                        ? 'No Date Chosen'
-                        : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}'),
-                  ),
-                  FlatButton(
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: platform
+                ? [
+                    CupertinoTextField(
+                      placeholder: 'Title',
+                      onSubmitted: (_) => _submitData(),
+                      controller: _titleController,
                     ),
-                    onPressed: _presentDatePicker,
-                  )
-                ],
-              ),
-            ),
-            RaisedButton(
-              onPressed: _submitData,
-              child: Text("Add transaction"),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-            )
-          ],
+                    SizedBox(height: 10),
+                    CupertinoTextField(
+                      placeholder: 'Amount',
+                      onSubmitted: (_) => _submitData(),
+                      controller: _amountController,
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(_selectedDate == null
+                                ? 'No Date Chosen'
+                                : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}'),
+                          ),
+                          AdaptiveFlatButton('Choose Date', _presentDatePicker)
+                        ],
+                      ),
+                    ),
+                    Center(
+                      child: CupertinoButton(
+                        onPressed: _submitData,
+                        child: Text("Add transaction"),
+                        color: Theme.of(context).primaryColor,
+                        // textColor: Theme.of(context).textTheme.button.color,
+                      ),
+                    )
+                  ]
+                : [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      onSubmitted: (_) => _submitData(),
+                      controller: _titleController,
+                      // onChanged: (value) => titleInput = value,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: _amountController,
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      onSubmitted: (_) => _submitData(),
+                      // onChanged: (value) => amountInput = value,
+                    ),
+                    Container(
+                      height: 70,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(_selectedDate == null
+                                ? 'No Date Chosen'
+                                : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}'),
+                          ),
+                          AdaptiveFlatButton('Choose Date', _presentDatePicker)
+                        ],
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: _submitData,
+                      child: Text("Add transaction"),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Theme.of(context).textTheme.button.color,
+                    )
+                  ],
+          ),
         ),
       ),
     );
